4c26ea5d53cd7c826773671dad8aade76fff34c4
manoj <manojsr032001@gmail.com>
2025-06-28 17:55:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import express from "express";
 import cors from "cors";
 import mysql from "mysql2";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+const express = require("express");
+
 
 dotenv.config();
 
